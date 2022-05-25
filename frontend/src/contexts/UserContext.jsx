53e0cd1004286b5d389d01f5ccafb7855cac2c14
ClementPremartin <clementpremartin@gmail.com>
2022-05-25 18:25:48 +0200

    create context and disable react props type missing in validation rule in eslint

---
+++ frontend/src/contexts/UserContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+function ContextProvider({ children }) {
+  const [test, setTest] = useState("coucou");
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <UserContext.Provider value={{ test, setTest }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+const userExport = { UserContext, ContextProvider };
+
+export default userExport;
