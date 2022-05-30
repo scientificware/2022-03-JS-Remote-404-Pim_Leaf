945d03abb236a41b3f8ef49305d91e029f272cca
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-30 15:01:59 +0200

    Adding data product in calvindata file

---
+++ frontend/src/data/CalvinData.jsx
@@ -0,0 +1,22 @@
+const productData = [
+  {
+    product: "Farine T65",
+    fabricant: "Moulin d'ici",
+    category: "Farines",
+    disponiblity: true,
+  },
+  {
+    product: "Pâtes torsades",
+    fabricant: "PastaBio",
+    category: "Pâtes et riz",
+    disponiblity: true,
+  },
+  {
+    product: "Huile d'olive extra-vierge",
+    fabricant: "Oilthentic",
+    category: "Huiles",
+    disponiblity: false,
+  },
+];
+
+export default productData;
