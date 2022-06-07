d657c409183841241ed3e6fa35d4c567f77ae982
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-06-07 15:02:58 +0200

    center the title products page

--- frontend/src/components/TitleProducts.jsx
+++ frontend/src/components/TitleProducts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TitleProducts() {
   return (
-    <div className="text-3xl">
+    <div className="flex justify-center text-3xl">
       <h1>Mes fiches produit</h1>
     </div>
   );
