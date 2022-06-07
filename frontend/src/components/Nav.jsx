710d5ab17b4aea8aa72a56e3d7e162022c91840b
Marie_Tignol <marie-tignol_student2022@wilder.school>
2022-06-07 23:03:41 +0200

    completed/add code for the responsive navbar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,46 +1,63 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Nav() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
-    <nav className="flex items-center justify-between pt-5">
+    <nav className="flex items-center justify-between">
       <img
         src="./src/assets/logo1.png"
         alt="logo"
-        className="h-28 ml-9 mb-5 item-center"
+        className="h-24 ml-7 mb-5 item-centers"
       />
-      <ul className="bg-white md:text-right w-screen h-screen md:pl-10 md:static fixed duration-500 ease-linear top-0 md:h-auto z-10">
-        <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
+      <img
+        src={
+          navOpen
+            ? "./src/assets/burger_close_icon.png"
+            : "./src/assets/burger_icon.png"
+        }
+        alt=""
+        className="h-5 z-20 fixed right-9 top-10 cursor-pointer lg:hidden"
+        onClick={() => setNavOpen(!navOpen)}
+      />
+      <ul
+        className={`bg-white lg:text-right w-full h-full duration-500 ease-linear lg:pl-10 lg:static fixed  top-0 lg:h-auto z-10 ${
+          !navOpen ? "right-[-100%]" : "right-0"
+        } `}
+      >
+        <li className="lg:inline-block  lg:ml-10 ml-5 lg:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
           <Link
             to="/products"
-            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block md:py-5 py-3"
+            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block lg:py-5 py-3"
           >
             <span className="font-bold mr-1.5">01</span>
             MES PRODUITS
           </Link>
         </li>
-        <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
+        <li className="lg:inline-block  lg:ml-10 ml-5 lg:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
           <Link
             to="/suppliers"
-            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block md:py-5 py-3"
+            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block lg:py-5 py-3"
           >
             <span className="font-bold mr-1.5">02</span>
             MES FOURNISSEURS
           </Link>
         </li>
-        <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
+        <li className="lg:inline-block  lg:ml-10 ml-5 lg:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
           <Link
             to="/company"
-            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block md:py-5 py-3"
+            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block lg:py-5 py-3"
           >
             <span className="font-bold mr-1.5">03</span>
             MON ENTREPRISE
           </Link>
         </li>
-        <li className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
+        <li className="lg:inline-block  lg:ml-10 ml-5 lg:my-0 my-6 border-b-2 border-transparent hover:border-white duration-300">
           <Link
             to="/profil"
-            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block md:py-5 py-3"
+            className="text-#14252F cursor-pointer font-Barlow font-normal text-sm inline-block lg:py-5 py-3"
           >
             <span className="font-bold mr-1.5">04</span>
             MON PROFIL
@@ -48,7 +65,7 @@ function Nav() {
         </li>
         <button
           type="button"
-          className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 hover:border-white duration-300"
+          className="lg:inline-block  lg:ml-10 ml-5 lg:my-0 my-6 hover:border-white duration-300"
         >
           <img
             src="./src/assets/logout_icon.png"
