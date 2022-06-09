536c326c38bce13c9909f28616d8be038ae6bc92
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-06-09 16:25:31 +0200

    import search bar component in page products

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -2,10 +2,12 @@
 import ListProducts from "@components/ListProducts";
 import TitleProducts from "@components/TitleProducts";
 import dataProducts from "../data/DataProducts";
+import SearchBarProducts from "../components/SearchBarProducts";
 
 function Products() {
   return (
     <main>
+      <SearchBarProducts />
       <TitleProducts />
       <ListProducts products={dataProducts} />
     </main>
