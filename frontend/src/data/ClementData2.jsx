1105400f0efd1f95459e60c5880345b791f7f424
ClementPremartin <clementpremartin@gmail.com>
2022-06-12 14:34:26 +0200

    install react hook form

---
+++ frontend/src/data/ClementData2.jsx
@@ -0,0 +1,12 @@
+const CompanyProfil = [
+  {
+    id: 0,
+    company_name: "Moulin d'ici",
+    address: "17 rue Bernard Moitessier",
+    city: "Brest",
+    mail: "moulinici@email.com",
+    phone: "0245236598",
+    domaine: "Farine",
+  },
+];
+export default CompanyProfil;
--- frontend/src/pages/Company.jsx
+++ frontend/src/pages/Company.jsx
@@ -1,17 +1,9 @@
-import { Link } from "react-router-dom";
-import dataCompany from "../data/DataCompany";
-
 function Company() {
   return (
-    <main id="Company" className="w-4/5 m-auto relative">
-      <h1>Company</h1>
-      <p>
-        {dataCompany.map((company) => (
-          <Link to={`/company/${company.id}`} key={company.id}>
-            <p>{company.name}</p>
-          </Link>
-        ))}
-      </p>
+    <main>
+      <h1 className="w-100 flex justify-center text-4xl text-darkBlue font-barlow ">
+        Mon entreprise
+      </h1>
     </main>
   );
 }
