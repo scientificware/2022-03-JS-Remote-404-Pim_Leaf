edaa315e006629b517c5607324ab34937a95cdf7
ClementPremartin <clementpremartin@gmail.com>
2022-06-22 15:58:32 +0200

    adding shadows on product detail

--- frontend/src/components/ProductsDetailsRetailer.jsx
+++ frontend/src/components/ProductsDetailsRetailer.jsx
@@ -3,7 +3,7 @@ function ProductsDetailsRetailer({ advise, recipeIdea }) {
     <div className="flex flex-col mt-20 font-redHat w-3/4 m-auto ">
       <h2 className="text-2xl">Mon commerce</h2>
       {/* COMPANY */}
-      <article className="bg-middleBlue/50 mb-10 flex justify-between">
+      <article className="bg-middleBlue/50 mb-10 flex justify-between shadow-lg">
         <div className="p-10">
           <h3 className="text-2xl pb-2">Astuces</h3>
           <p>{advise}</p>
