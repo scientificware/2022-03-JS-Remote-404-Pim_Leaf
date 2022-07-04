053fd1faf03d027e1117c27d91a6adf99bf221a4
ClementPremartin <clementpremartin@gmail.com>
2022-07-04 16:12:18 +0200

    modify dashboardLayout files to clean code

--- frontend/src/components/layout/DashBoardLayoutSupplier.jsx
+++ frontend/src/components/layout/DashBoardLayoutSupplier.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable import/no-unresolved */
-
 import { Outlet } from "react-router-dom";
 
-import React from "react";
 import Nav from "@components/Nav";
 
 function DashboardLayoutSupplier() {
