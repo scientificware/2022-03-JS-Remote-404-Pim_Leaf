c894d0f2ab540f2c845369b439cd0dd1da5c9945
Benoît V <benoit@vandanjon.fr>
2022-07-06 22:29:44 +0200

    move company and profil pages to common + due changes on FormField call + correct typo on Nav

---
+++ frontend/src/pages/common/Profil.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-unresolved */
+import Fields from "@components/common/Fields";
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+
+import UserExport from "@contexts/UserContext";
+
+function Profil() {
+  const { user } = useContext(UserExport.UserContext);
+
+  const [setDatas] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}user/${user.company_group_id}`)
+      .then((res) => {
+        setDatas(res.data);
+      });
+  }, []);
+
+  return (
+    <main>
+      <h1 className="flex justify-center text-3xl font-bold font-barlow text-darkBlue mb-14 mt-14">
+        Mon profil
+      </h1>
+      <Fields />
+    </main>
+  );
+}
+
+export default Profil;
