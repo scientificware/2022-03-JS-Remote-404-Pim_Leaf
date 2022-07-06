7bd72ce792e158122d409e35168256005d9e1c01
Benoît V <benoit@vandanjon.fr>
2022-07-06 10:36:29 +0200

    add axios + useeffect on productstable suppliers

--- frontend/src/components/suppliers/ProductsTable.jsx
+++ frontend/src/components/suppliers/ProductsTable.jsx
@@ -1,5 +1,33 @@
-function ProductsTable() {
-  return <main>productstable</main>;
-}
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+function ProductsTable() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}products`)
+      .then((res) => {
+        const prod = res.data.map((el) => ({ ...el, check: false }));
+        setProducts(prod);
+      })
+      .catch((error) => {
+        console.warn(error.response.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="ml-12">
+        <p>
+          Connecté en tant que : <b>Eco Vrac</b>
+        </p>
+        <p>
+          Produits de : <b>Nat-ali</b> et <b>Epice Scop</b>
+        </p>
+      </div>
+      <main>{products}</main>
+    </>
+  );
+}
 export default ProductsTable;
