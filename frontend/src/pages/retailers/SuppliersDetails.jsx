14fb7d726e58674caea8c7c334cd2576b79ecde1
Benoît V <benoit@vandanjon.fr>
2022-07-09 11:56:04 +0200

    revamp ProductsDetailsSupplier + add SuppliersDetailsDescrtiption and setup SuppliersDetails front

--- frontend/src/pages/retailers/SuppliersDetails.jsx
+++ frontend/src/pages/retailers/SuppliersDetails.jsx
@@ -1,10 +1,42 @@
 /* eslint-disable import/no-unresolved */
+import { useParams } from "react-router-dom";
+
+import SearchBar from "@components/common/SearchBar";
+import ProductsDetailsSupplier from "@components/retailers/ProductsDetailsSupplier";
+import SuppliersDetailsDescription from "@components/retailers/SuppliersDetailsDescription";
+
+import axios from "axios";
+import { useState, useContext, useEffect } from "react";
+
+import UserExport from "@contexts/UserContext";
 
 function SuppliersDetails() {
+  const { id } = useParams();
+  const { user } = useContext(UserExport.UserContext);
+  const [supplier, setSupplier] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/company/${id}`)
+      .then((res) => setSupplier(res.data));
+  }, []);
+
+  console.warn(user);
   return (
     <main>
-      <p className="text-5xl flex justify-center pt-10">suppliers details</p>
-      <div className="bg-middleBlue/70 relative m-10 mt-16">test</div>
+      <h1 className="flex justify-center text-3xl font-bold font-barlow">
+        {supplier && supplier.company_name}
+      </h1>
+
+      <div className="flex flex-col font-redHat w-3/4 m-auto">
+        <SuppliersDetailsDescription supplier={supplier} />
+
+        <ProductsDetailsSupplier />
+      </div>
+
+      <SearchBar />
+      {/* <ButtonFunction /> */}
+      {/* <DetailsSuppliers products={dataProducts} /> */}
     </main>
   );
 }
