5cf44f97b5e00b3a32db8344ce29d38df7308811
ClementPremartin <clementpremartin@gmail.com>
2022-07-12 11:19:51 +0200

    filter the suppliers in modal

--- frontend/src/components/retailers/ModalAddSuppliers.jsx
+++ frontend/src/components/retailers/ModalAddSuppliers.jsx
@@ -4,8 +4,27 @@
 /* eslint-disable react/destructuring-assignment */
 
 import SuppliersListModal from "@components/retailers/SuppliersListModal";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function ModalAddSuppliers({ suppliers, searchInput }) {
+function ModalAddSuppliers({ searchInput, connected, pending }) {
+  const [suppliers, setSuppliers] = useState([]);
+
+  const connectId = connected.map((connect) => connect.supplier_id);
+  const pendId = pending.map((pend) => pend.supplier_id);
+  const compare = [...connectId, ...pendId];
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}retailer/suppliers`)
+      .then((res) => {
+        setSuppliers(res.data);
+      })
+      .catch((error) => {
+        console.warn(error.response.data);
+      });
+  }, []);
+
   return (
     <div className="pl-10 pr-10 pb-10">
       <table>
@@ -25,6 +44,7 @@ function ModalAddSuppliers({ suppliers, searchInput }) {
         </thead>
         <tbody>
           {suppliers
+            .filter((supplier) => !compare.includes(supplier.company_id))
             .filter(
               (supplier) =>
                 supplier.company_name.includes(searchInput) ||
